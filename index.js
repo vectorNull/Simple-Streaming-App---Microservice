e1f21d755a0a789423ae16b26b2cd4160c43eea2
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-01-16 16:46:46 -0800

    Create readStream for local video

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
-
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/video', (req, res) => {
+    
+    const path = "../simpleStreamingApp_Microservice/videos/pexel_bluberries.mp4";
+    fs.stat(path, (err, stats) => {
+        if (err) {
+            console.error('An error occured.');
+            res.sendStatus(500);
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4"
+        });
+
+        fs.createReadStream(path).pipe(res);
+    })
 })
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}; point your browser at http://localhost:3000/video`);
 })
